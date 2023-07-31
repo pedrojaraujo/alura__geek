79a1c19f470288fec543a6c74e32a3e43bb450d8
pedrojaraujo <pedroa990@gmail.com>
2023-07-31 19:43:46 -0300

    update .env

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACjCRtqRq6d_3ou25SUJcI93f8FhWItBM",
+  authDomain: "geekproject-4fa69.firebaseapp.com",
+  projectId: "geekproject-4fa69",
+  storageBucket: "geekproject-4fa69.appspot.com",
+  messagingSenderId: "863126915377",
+  appId: "1:863126915377:web:03f5b4ed6eb08ddbd1c810",
+  measurementId: "G-S6YQTT79Y4",
+};
+
+const app = initializeApp(firebaseConfig);
